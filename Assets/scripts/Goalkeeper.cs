8175e70797e716e87c5bbd98882fa21c2df705cd
Ictis26 <vincent.remlinger.vr@gmail.com>
2020-09-27 23:17:23 +0200

    Goalkeeper try

---
+++ Assets/scripts/Goalkeeper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goalkeeper : MonoBehaviour
+{
+    public float walkSpeed = 9;
+    public float sidewaysSpeed = 6;
+    public float speed = 50;
+    private float timesincestart = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        float vertical_movement = Mathf.Sin(speed * timesincestart) * walkSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical_movement);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
